221928ea1e057027309170f16f6d4d72528614f3
thomascritelli <thomas.critelli@alessandrinimainardi.edu.it>
2024-02-01 17:52:42 +0100

    pagella finita

--- sockets/esercizi/1_pagella/v1/client.py
+++ sockets/esercizi/1_pagella/v1/client.py
@@ -1,33 +1,38 @@
-import socket, json
+import socket
+import json
 
 SERVER_IP = '127.0.0.1'
-SERVER_PORT = 5005
+SERVER_PORT = 22004
 BUFFER_SIZE = 1024
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((SERVER_IP, SERVER_PORT))
 
 while True:
     comando = input("Inserisci comando: ")
     parametri = []
+    materia = ""
+    voto = 0
+    ore = 0
+
     if comando != "list" and comando != "close":
-        nomeStudente = input("Inserisci il nome e cognome dello studente").lower()
-        nomeStudente = nomeStudente.strip()
-        parametri.append(nomeStudente)
+        nomeStudente = input("Inserisci il nome e cognome dello studente: ").lower()
+
         if comando == "put":
             materia = input("Inserisci il nome della materia: ").lower().strip()
-            
             voto = float(input("Inserisci il voto:"))
-            
             ore = int(input("Inserisci il numero di ore per assenza: "))
-        parametri.append(materia)
-        parametri.append(voto)
-        parametri.append(ore)
-        
-    comando = json.dumps(comando)
-    parametri = json.dumps(input("Inserisci parametri: "))
+        parametri.extend([nomeStudente, materia, voto, ore])
+
+    else:
+        if comando == "close":
+            break
     istruzioni = {
-        'comando':comando,
-        'parametri':parametri,
+        'comando': comando,
+        'parametri': parametri,
     }
-    sock.sendto(comando.encode("UTF-8"),(SERVER_IP,SERVER_PORT))
+    istruzioni = json.dumps(istruzioni)
+    sock.sendall(istruzioni.encode("UTF-8"))
+
     data = sock.recv(BUFFER_SIZE)
+    print("Risposta dal server:", data.decode("UTF-8"))
--- sockets/esercizi/1_pagella/v1/server.py
+++ sockets/esercizi/1_pagella/v1/server.py
@@ -6,9 +6,9 @@ SERVER_PORT = 22004
 BUFFER_SIZE = 1024
 
 diz = {'antonio barbera': [['matematica', 8, 1], ['italiano', 6, 1], ['inglese', 9.5, 0], ['storia', 8, 2], ['geografia', 8, 1]],
-    'giuseppe gullo': [['matematica', 9, 0], ['italiano', 7, 3], ['inglese', 7.5, 4], ['storia', 7.5, 4], ['geografia', 5, 7]],
-    'nicola spina': [['matematica', 7.5, 2], ['italiano', 6, 2], ['inglese', 4, 3], ['storia', 8.5, 2], ['geografia', 8, 2]],
-    }
+       'giuseppe gullo': [['matematica', 9, 0], ['italiano', 7, 3], ['inglese', 7.5, 4], ['storia', 7.5, 4], ['geografia', 5, 7]],
+       'nicola spina': [['matematica', 7.5, 2], ['italiano', 6, 2], ['inglese', 4, 3], ['storia', 8.5, 2], ['geografia', 8, 2]],
+      }
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock_server:
     sock_server.bind((SERVER_IP, SERVER_PORT))
@@ -17,7 +17,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock_server:
 
     while True:
         sock_service, address_client = sock_server.accept()
-        data=sock_service.recv(BUFFER_SIZE)
+        data = sock_service.recv(BUFFER_SIZE)
+
         if not data:
             break
         else:
@@ -25,52 +26,37 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock_server:
             data = json.loads(data)
 
             # Assegnamento dei dati ad una variabile
-            comando = data[comando]
-            parametri = data[parametri]
+            comando = data['comando']
+            parametri = data['parametri']
 
             # Comando #list
-            if comando == "#list":
+            if comando == "list":
                 risp = ("OK", diz)
 
             # Comando #get
-            elif comando == "#get":
-                if parametri[0] in diz.items():
-                    studente = diz[0]
-                else:
-                    print("Studente non trovato!")
-            
-            # Comando #set
-            elif comando =="#set":
+            elif comando == "get":
                 if parametri[0] in diz:
-                    print("Studente già presente!")
+                    studente = diz[parametri[0]]
+                    risp = ("OK", studente)
                 else:
-                    diz.append(studente)
-            
-            
+                    risp = ("Studente non trovato!", None)
 
-
-                
-
-        with sock_service as sock_client:
-            data=data.decode()
-            data=json.loads(data)
-            primoNumero=data['primoNumero']
-            operazione=data['operazione']
-            secondoNumero=data['secondoNumero']
-
-            if(operazione != 'x' and operazione != '+' and operazione != '-' and operazione != '/'):
-                print("operatore non accettabile")
-                break
-            else:
-                if(operazione == 'x'):
-                    risultato=primoNumero*secondoNumero
-                elif(operazione == '+'):
-                    risultato=primoNumero+secondoNumero
-                elif(operazione == '-'):
-                    risultato=primoNumero-secondoNumero
+            # Comando #set
+            elif comando == "set":
+                if parametri[0] in diz:
+                    risp = ("Studente già presente!", None)
                 else:
-                    risultato=primoNumero/secondoNumero
-
-                sock_service.sendall((str(risultato)).encode())
+                    diz[parametri[0]] = parametri[1:]
+                    risp = ("OK", diz)
             
-            print(risultato)
+            elif comando == "put":
+                materia = parametri[1]
+                voto = parametri[2]
+                ore = parametri[3]
+                if parametri[0] in diz:
+                    diz[parametri[0]].append([materia, voto, ore])
+                else:
+                    risp = ("Studente non trovato!", None)
+
+
+            sock_service.sendall(json.dumps(risp).encode())
