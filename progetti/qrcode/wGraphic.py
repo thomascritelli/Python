a040bd137d83bf5ef1d25efc00cd8e7c54d095ea
thomascritelli <thomas.critelli@alessandrinimainardi.edu.it>
2024-02-22 06:56:27 +0100

    fixed view

--- progetti/qrcode/wGraphic.py
+++ progetti/qrcode/wGraphic.py
@@ -1,31 +1,37 @@
 import tkinter as tk
 from tkinter import ttk
-from PIL import Image, ImageTk
+from PIL import Image
 import qrcode
+import cv2
+
+def getInfo(textUrl, textNme):
+    nome = textNome.get()
+    url = textUrl.get()
+    textNome.set("")
+    textUrl.set("")
+    generate_qr_code(url, nome)
+    show_qr_code(nome)
 
 # funzione per generare il qrcode
 def generate_qr_code(url, nome):
     img = qrcode.make(url)
     img.save(f'{nome}.png')
-    show_qr_code(nome)
 
-# funzione per visualizzare il qr code nella finestra
+# funzione per visualizzare il qr code
 def show_qr_code(nome):
-    image = Image.open(f'{nome}.png')
-    photo = ImageTk.PhotoImage(image)
-    qr_label.config(image=photo)
-    qr_label.image = photo
+    img = Image.open(f'{nome}.png')
+    img.show()
 
 # root window
 root = tk.Tk()
-root.geometry('900x700')
+root.geometry('300x200')
 root.title('QR Code Generator')
 
 # Entry
-nome = tk.StringVar()
-url = tk.StringVar()
-textboxNome = ttk.Entry(root, textvariable=nome)
-textboxUrl = ttk.Entry(root, textvariable=url)
+textNome = tk.StringVar()
+textUrl = tk.StringVar()
+textboxNome = ttk.Entry(root, textvariable=textNome)
+textboxUrl = ttk.Entry(root, textvariable=textUrl)
 
 textboxNome.pack(
     ipadx=5,
@@ -43,7 +49,7 @@ textboxUrl.pack(
 ok_button = ttk.Button(
     root,
     text='Genera',
-    command=lambda: generate_qr_code(url.get(), nome.get())
+    command=lambda: getInfo(textUrl, textNome)
 )
 
 ok_button.pack(
